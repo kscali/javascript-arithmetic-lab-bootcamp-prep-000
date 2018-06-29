2d5bd0ad915f1ef7a50527ddf5c4586f82c1351e
kosatun@aol.com <kosatun@aol.com>
2018-06-29 03:56:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 function add() {
   return a + b
 }
+
+function subtract() {
+  return a - b
+}
+
+function multiply() {
+  return a * b
+}
+
+function divide() {
+  return a / b
+}
+
+function inc(n) {
+  
+}
+
+function dec(n) {
+  
+}
+
+function makeInt(n) {
+  
+}
+
+function preserveDecimal(n) {
